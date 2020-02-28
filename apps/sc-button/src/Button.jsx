590a7bd4749b6550fa1217780b7b0b57dc3b6fbe
Cody Mathisen <cody.mathisen@epicgames.com>
2020-02-27 22:03:10 -0800

    update sc button css props

--- apps/sc-button/src/Button.jsx
+++ apps/sc-button/src/Button.jsx
@@ -1,10 +1,13 @@
 import styled from "styled-components";
 
 export const Button = styled.button`
+  display: inline-block;
+  border-radius: 3px;
+  padding: 0.5rem 0;
+  margin: 0.5rem 1rem;
+  width: 11rem;
   background: palevioletred;
   color: white;
-  font-size: 1em;
-  margin: 1em;
-  padding: 0.25em 1em;
-  border-radius: 3px;
+  font-size: 1rem;
+  font-family: sans-serif;
 `;
