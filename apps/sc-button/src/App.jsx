af1d66211963905eae35fc7a7969f4d4adeac95a
Cody Mathisen <cody.mathisen@epicgames.com>
2020-02-27 21:59:08 -0800

    add styled-components button app

---
+++ apps/sc-button/src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from "./Button";
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Styled Component Button</h1>
+      <Button>SC Button</Button>
+    </div>
+  );
+}
+
+export default App;
---
+++ apps/sc-button/src/Button.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  background: palevioletred;
+  color: white;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border-radius: 3px;
+`;
